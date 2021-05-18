6d3019cb26592f47a1706050112765e277f4ab12
Nuno Alves <nyansword@gmail.com>
2021-05-18 11:22:54 +0100

    add: person name to query

--- part3/int1.sql
+++ part3/int1.sql
@@ -5,8 +5,9 @@ SELECT cp1.purchases AS purchases
 FROM client_purchases AS cp1, client_purchases AS cp2
 WHERE cp1.purchases < cp2.purchases;
 
-SELECT id, purchases
+SELECT id, person.name as person_name, purchases
 FROM client_purchases
+JOIN person USING(id)
 WHERE purchases NOT IN (
 	SELECT purchases FROM purchases_lower_than_any
 );
