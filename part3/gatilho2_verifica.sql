1bc543516268e4a7c64623a7f48e003e5132d528
Andre Moreira <andre.moreira.9@hotmail.com>
2021-05-23 00:11:02 +0100

    Trigger now selecting warehouse with the highest total stock

---
+++ part3/gatilho2_verifica.sql
@@ -0,0 +1,14 @@
+.read gatilho2_adiciona.sql
+
+SELECT "SHOULD FAIL";
+insert into "order" (id, employee_id, status) values (12, NULL, 'waiting'); -- Should fail
+
+SELECT "BEFORE INSERT";
+SELECT * FROM stock WHERE storage_id = 21;
+SELECT * FROM cart_quantity WHERE cart_quantity.cart_id=11;
+insert into "order" (id, employee_id, status) values (11, NULL, 'waiting');
+SELECT "AFTER INSERT";
+SELECT * FROM stock WHERE storage_id = 21;
+SELECT * FROM cart_quantity WHERE cart_quantity.cart_id=11;
+
+.read gatilho2_remove.sql
--- part3/trigger/gatilho2_verifica.sql
+++
@@ -1,2 +0,0 @@
-insert into "order" (id, employee_id, status) values (12, NULL, 'waiting');
-insert into "order" (id, employee_id, status) values (11, NULL, 'waiting');
