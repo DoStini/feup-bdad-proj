c0e04bb2bff6d9fd2cb531b04f47e50470187c8a
Nuno Alves <nyansword@gmail.com>
2021-05-23 23:42:38 +0100

    better 3 check

--- part3/gatilho3_verifica.sql
+++ part3/gatilho3_verifica.sql
@@ -1,1 +1,18 @@
+select "SHOULD FAIL";
+
 insert into shipment (id, shipment_date, reception_date, distance, address_id, shipment_type_id) values (2, '2022-04-20 13:50:36', null, 693.69008, 63, 3);
+
+select "BEFORE INSERT";
+
+SELECT * FROM shipment;
+
+insert into cart (id, client_id) values(510, 1);
+insert into "order" (id, employee_id, status) values(510, 33, 'processing');
+insert into payment_mb_way (id, payment_value, payment_phone_number) values(510, 10, 10);
+
+
+insert into shipment (id, shipment_date, reception_date, distance, address_id, shipment_type_id) values (510, '2022-04-20 13:50:36', null, 693.69008, 63, 3);
+
+select "AFTER INSERT";
+
+select * FROM shipment;
